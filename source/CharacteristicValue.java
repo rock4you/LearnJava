7c2c2f031157613ca5e3c213bbb74f13d700c3a9
rock4you <rockpeng@foxmail.com>
2017-05-11 15:22:50 +0800

    3.1 Calculate the characteristic value of a number

---
+++ source/CharacteristicValue.java
@@ -0,0 +1,42 @@
+import java.util.*;
+// calculate the characteristic value
+public class CharacteristicValue {
+	public static void main(String[] args)
+	{
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		int num = in.nextInt();
+		int i=1;
+		int j=10;
+		int tmp=0;
+		int result=0;
+		int index=0;
+		index=num/((int) Math.pow(10,i-1) );
+		while( index!=0 )
+		{	
+			tmp=(num%j)/((int) Math.pow(10,i-1) );
+			if(i%2==0)
+			{
+				if(tmp%2==0)
+				{
+					result+=(int)Math.pow(2, i-1);
+				}
+			
+			}
+			else
+			{
+				if(tmp%2==1)
+				{
+					result+=(int)Math.pow(2, i-1);
+				}
+			}
+			
+			i=i+1;
+			j=j*10;
+			index=num/((int) Math.pow(10,i-1) );
+			
+		}	
+		System.out.print(result);
+	}
+	
+}
